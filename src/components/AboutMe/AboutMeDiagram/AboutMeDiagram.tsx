deb4a9145c66de1d3439a9133411357818be5e77
Fiji <radek42310o@gmail.com>
2023-03-12 13:42:23 +0100

    fill dummy descriptions, add site logo

--- src/components/AboutMe/AboutMeDiagram/AboutMeDiagram.tsx
+++ src/components/AboutMe/AboutMeDiagram/AboutMeDiagram.tsx
@@ -5,11 +5,11 @@ const ABOUT_ME_INFO = [
     { title: 'Username', desc: 'Fiji404 or Radek :D' },
     {
         title: 'Description',
-        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque corporis accusantium tenetur? Nam aperiam ipsa commodi ea voluptate saepe laboriosam!',
+        desc: 'I am an eighteen-year-old from Poland who is interested in programming. I have been fascinated by technology and the way the internet works from a young age. Soon, I began to learn the ins and outs of programming, and I am currently developing my skills in this area. My passion is developing web applications and improving their functionality. I always try to learn new technologies and tools to create better and more innovative solutions. I want to develop my programming skills to work as a programmer in the future and contribute to the development of the IT industry.',
     },
     {
         title: 'My career',
-        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore ab esse, totam consequaturanimi ullam explicabo velit ipsam, quae assumenda veniam in, eius illum tenetur?',
+        desc: 'Towards the end of 2021, I embarked on the journey of creating websites. I began by selecting two fundamental languages - HTML and CSS, and acquired knowledge through online tutorials on YouTube and official documentation, implementing my learning into personal projects. Later, I advanced my skills by learning JavaScript and TypeScript through comprehensive courses on Udemy, supplemented with personal projects. I then extended my knowledge by studying SASS and TailwindCSS. Presently, I am furthering my learning on React via Udemy.',
     },
     { title: 'Skills', desc: <KnownLanguages /> },
 ];
--- src/components/AboutMe/KnownLanguages/KnownLanguage.tsx
+++ src/components/AboutMe/KnownLanguages/KnownLanguage.tsx
@@ -4,9 +4,9 @@ interface Props {
     langIcon: React.ReactNode;
 }
 
-export const KnownLanguage = ({lang, langWebsite, langIcon}: Props) => {
+export const KnownLanguage = ({ lang, langWebsite, langIcon }: Props) => {
     return (
-        <li className="grow max-w-[200px]">
+        <li className="grow">
             <a href={langWebsite} data-lang={lang.toLowerCase()} target="_blank" className="skill-item">
                 {lang} {langIcon}
             </a>
